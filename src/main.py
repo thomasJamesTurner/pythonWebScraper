9b4db04429fad84334724ae5df4827244b8050bb
Thomas Turner <thomas.james.turner22122004@gmail.com>
2024-11-18 21:02:07 +0000

    added gitignore

--- src/main.py
+++ src/main.py
@@ -1,23 +1,7 @@
-from urllib.request import urlopen
 import pandas as pd
 from io import StringIO
 import requests
-import re
 
-def get_title(html):
-    pattern = "<title.*?>.*?</title.*?>"
-    match_results = re.search(pattern, html, re.IGNORECASE)
-    title = match_results.group()
-    title = re.sub("<.*?>", "", title) # Remove HTML tags
-    return title
-
-def get_info(html):
-    data = re.findall("<td[^>]*>.*?<b[^>]*>(.*?)</b>.*?</td>",html)
-    text = ""
-    for item in data:
-        text += re.sub("<.*?>", "", item)
-        text += "\n"
-    return text
 
 url = "https://finviz.com/quote.ashx?t=NEXT&ty=c&ta=1&p=d"
 headers = {
@@ -26,12 +10,11 @@ headers = {
 page = requests.get(url,allow_redirects=True, headers=headers)
 html_io = StringIO(page.text)
 dfs = pd.read_html(html_io)
-
+df = dfs[7]
+df = df.transpose()
 
 
 
 print(dfs[7])
-#print(get_title(html))
-#print(get_info(html))
 
 
